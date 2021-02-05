448b3298b0d87f7926b7f9281baad4f123ea740b
AlanNggg <ngpuilam9166@gmail.com>
2021-02-06 04:17:06 +0800

    refactoring showcase css

--- script/script.js
+++ script/script.js
@@ -1,28 +1,35 @@
-const showcase_back = document.querySelector('.showcase_button_back');
-const showcase_next = document.querySelector('.showcase_button_next');
-const showcase_item_active = document.querySelector('.active');
-const showcase_items = document.querySelectorAll('.showcase_item');
+const showcase_button_back = document.querySelector(".showcase_button_back");
+const showcase_button_next = document.querySelector(".showcase_button_next");
+const showcase_items = document.querySelectorAll(".showcase_item");
 
-function isActiveShowcaseItem (el) {
-    return el.classList.contains('active');
+function isActiveShowcaseItem(el) {
+  return el.classList.contains("active");
 }
 
-showcase_back.addEventListener('click', () => {
-    const showcase_items_array = [...showcase_items];
-    const show_item_active_index = showcase_items_array.findIndex(isActiveShowcaseItem);
-    
-    if (show_item_active_index > 0) {
-        showcase_items_array[show_item_active_index].classList.toggle('active');
-        showcase_items_array[show_item_active_index-1].classList.toggle('active');
-    }
+showcase_button_back.addEventListener("click", () => {
+  const showcase_items_array = [...showcase_items];
+  const show_item_active_index = showcase_items_array.findIndex(
+    isActiveShowcaseItem
+  );
+
+  if (show_item_active_index > 0) {
+    showcase_items_array[show_item_active_index].classList.remove("active");
+    showcase_items_array[show_item_active_index].classList.add("next");
+    showcase_items_array[show_item_active_index - 1].classList.add("active");
+    showcase_items_array[show_item_active_index - 1].classList.remove("prev");
+  }
 });
 
-showcase_next.addEventListener('click', () => {
-    const showcase_items_array = [...showcase_items];
-    const show_item_active_index = showcase_items_array.findIndex(isActiveShowcaseItem);
-    
-    if (show_item_active_index < showcase_items_array.length - 1) {
-        showcase_items_array[show_item_active_index].classList.toggle('active');
-        showcase_items_array[show_item_active_index+1].classList.toggle('active');
-    }
+showcase_button_next.addEventListener("click", () => {
+  const showcase_items_array = [...showcase_items];
+  const show_item_active_index = showcase_items_array.findIndex(
+    isActiveShowcaseItem
+  );
+
+  if (show_item_active_index < showcase_items_array.length - 1) {
+    showcase_items_array[show_item_active_index].classList.remove("active");
+    showcase_items_array[show_item_active_index].classList.add("prev");
+    showcase_items_array[show_item_active_index + 1].classList.add("active");
+    showcase_items_array[show_item_active_index + 1].classList.remove("next");
+  }
 });
